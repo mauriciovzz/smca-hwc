47153c22d0886dbcc11a44e4ec08ac34703dee09
mauriciovzz <mauriciovr12@gmail.com>
2023-07-12 15:35:27 -0400

    Add serial communication between arduino and esp

--- ArduinoCodeIndoor/ArduinoCodeIndoor.ino
+++ ArduinoCodeIndoor/ArduinoCodeIndoor.ino
@@ -1,9 +1,25 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <SoftwareSerial.h>
+#include <DHT.h> 
 
+// Serial Communication --------------------------------------------------------------
+const byte rxPin = 10;
+const byte txPin = 11;
+SoftwareSerial Arduino_Serial (rxPin, txPin);
+
+void sendSerialData(const char *message){
+  Serial.println(message);
+  Arduino_Serial.print(message);
+}
+
+// -----------------------------------------------------------------------------------
+void setup() {
+  Serial.begin(57600);
+  Arduino_Serial.begin(9600);
+
+  Serial.println("<Arduino is ready>");
+
+  sendSerialData("<Mensaje desde arduino>");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
 }
--- ESPCode/ESPCode.ino
+++ ESPCode/ESPCode.ino
@@ -1,9 +1,65 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <SoftwareSerial.h>
 
+// Serial Communication --------------------------------------------------------------
+const byte rxPin = D1;
+const byte txPin = D2;
+SoftwareSerial ESP_Serial(rxPin, txPin);
+
+
+const byte numChars = 32;
+char receivedChars[numChars];
+char tempChars[numChars];       
+
+char recvType[numChars] = {0};
+char recvValue[numChars] = {0};
+
+boolean newData = false;
+
+void recvSerialData(){
+  static boolean recvInProgress = false;
+  static byte ndx = 0;
+  char startMarker = '<';
+  char endMarker = '>';
+  char rc;
+
+  while(ESP_Serial.available() > 0 && newData == false){
+    rc = ESP_Serial.read();
+
+    if(recvInProgress == true){
+      if (rc != endMarker) {
+        receivedChars[ndx] = rc;
+        ndx++;
+        if (ndx >= numChars) {
+          ndx = numChars - 1;
+        }
+      }
+      else {
+        receivedChars[ndx] = '\0'; // terminate the string
+        recvInProgress = false;
+        ndx = 0;
+        newData = true;
+      }
+    }
+    else if (rc == startMarker) {
+      recvInProgress = true;
+    }
+  }
+
+  if (newData == true) {
+    Serial.println(receivedChars);
+    newData = false;
+  }
+}
+
+// -----------------------------------------------------------------------------------
+void setup() {
+  Serial.begin(57600);
+  ESP_Serial.begin(9600);
+
+  Serial.println("<ESP is ready>");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  recvSerialData();
 }
+
