ae04e0f16f1d22437d712fc97db524a1d6999327
mauriciovzz <mauriciovr12@gmail.com>
2023-07-14 14:20:10 -0400

    wifi support + serial data parser

--- ESPCode/ESPCode.ino
+++ ESPCode/ESPCode.ino
@@ -1,4 +1,31 @@
+#include <ESP8266WiFi.h>
 #include <SoftwareSerial.h>
+
+// WIFI ------------------------------------------------------------------------------
+const char* ssid = "iPhone";
+const char* password = "12345678";
+
+void setup_wifi() {
+  delay(10);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
 
 // Serial Communication --------------------------------------------------------------
 const byte rxPin = D1;
@@ -45,9 +72,29 @@ void recvSerialData(){
   }
 
   if (newData == true) {
-    Serial.println(receivedChars);
+    strcpy(tempChars, receivedChars);
+    parseData();
+    showParsedData();
+    //sendMqttData(recvType, recvValue);
     newData = false;
   }
+}
+
+void parseData() {     
+  char* strtokIndx; 
+
+  strtokIndx = strtok(tempChars,",");     
+  strcpy(recvType, strtokIndx); 
+
+  strtokIndx = strtok(NULL, ",");
+  strcpy(recvValue, strtokIndx); 
+}
+
+void showParsedData() {
+  Serial.print("Type: ");
+  Serial.print(recvType); 
+  Serial.print(" Value: ");
+  Serial.println(recvValue);
 }
 
 // -----------------------------------------------------------------------------------
@@ -55,10 +102,18 @@ void setup() {
   Serial.begin(57600);
   ESP_Serial.begin(9600);
 
+  setup_wifi();
+  //client.setServer(mqtt_server, mqtt_port);
+
   Serial.println("<ESP is ready>");
 }
 
 void loop() {
+  //if (!client.connected()) {
+  //  reconnect();
+  //}  
+  //client.loop();
+
   recvSerialData();
 }
 
